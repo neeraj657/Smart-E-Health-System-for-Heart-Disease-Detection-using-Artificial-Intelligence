0f7cfc949b66918bc1173669ccf824e3a42d801f
K.R. NEERAJ <krneerajnaidu@gmail.com>
2025-09-16 22:20:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,5 +4,6 @@ MYSQL_HOST = 'localhost'
 MYSQL_USER = 'root'
 MYSQL_PASSWORD = 'Manasa@2025'
 MYSQL_DB = 'heart_disease_db'
-SECRET_KEY = "3b36ca5a627a585e730f7f55a5b382038fa63993d1caecbd2e2a7109c54ab0e2"
-GEMINI_API_KEY = "AIzaSyCUAx04fdqEgXqy1HPDCuwnPwy0M7MVcOs"
+SECRET_KEY = "yyyyyyyyyyyyyyyyyyyyyyy"
+
+GEMINI_API_KEY = "xxxxxxxxxxxxxxxxxxxx"
